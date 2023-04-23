86dd864e447a4903bbba20ca3ba808c26dbd8130
Matt Groff <mattlgroff@gmail.com>
2023-04-23 14:18:34 -0400

    Update notice text and test for home controller

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,5 @@
 class AboutController < ApplicationController
   def index
-    flash[:notice] = "Your profile has been updated."
+    flash[:notice] = "You visited the About page and got this notice."
   end
 end
